644b227a7eba29b42516548bdd19191ba7f2b974
Jon Kjennbakken <jonkjenn@gmail.com>
2016-03-02 11:40:40 +0100

    stuff

--- src/personFollower.cpp
+++ src/personFollower.cpp
@@ -2,6 +2,7 @@
 #include <k2_client/k2_client.h>
 #include <k2_client/BodyArray.h>
 #include <geometry_msgs/Twist.h>
+#include <nav_msgs/Odometry.h>
 #include <tf/transform_listener.h>
 #include <signal.h>
 #include <string>
@@ -21,119 +22,124 @@ bool prevAboveLength = false;
 int underLenCount = 0;
 
 void watchdog(int sig){
-	vel_msg.linear.x = 0; 
-	vel_msg.linear.y = 0; 
-    vel_msg.linear.z = 0; 
-    vel_msg.angular.x = 0;
-    vel_msg.angular.y = 0;
-	vel_msg.angular.z = 0;
-	personPos_pub.publish(vel_msg);
+  vel_msg.linear.x = 0; 
+  vel_msg.linear.y = 0; 
+  vel_msg.linear.z = 0; 
+  vel_msg.angular.x = 0;
+  vel_msg.angular.y = 0;
+  vel_msg.angular.z = 0;
+  personPos_pub.publish(vel_msg);
 }
-	
+
+void pose_sub_cb(const nav_msgs::Odometry::ConstPtr& msg){
+  ROS_INFO_STREAM("Got odometry");
+}
+
 void bodies_sub_cb(const k2_client::BodyArray msg){
-    //ROS_INFO_NAMED("personFollower", "personFollower: Received bodyArray");
+  //ROS_INFO_NAMED("personFollower", "personFollower: Received bodyArray");
 
-	alarm(1);
-	//++count;
-	
-    vel_msg.linear.y = 0; 
-    vel_msg.linear.z = 0; 
-    vel_msg.angular.x = 0;
-    vel_msg.angular.y = 0;
+  alarm(1);
+  //++count;
 
-	// Continue to track the same person, or find a new person to track (if any)
-    if(s != -1){
-		if(msg.bodies[s].isTracked == 0){
-			s = -1;
-			ROS_INFO_NAMED("personFollower", "personFollower: tracking ended");
-		}
-	}
-	if(s == -1){
-		//if(count > 10){
-			vel_msg.linear.x = 0;
-			vel_msg.angular.z = 0;
-		//}
-		aboveAngle = false;
-		aboveLength = false;
-		for(int i = 0; i < 6; i++){
-			if(msg.bodies[i].isTracked == 1){
-				s = i;
-				ROS_INFO_NAMED("personFollower", "personFollower: tracking new person");
-				break;
-			}
-		}
+  vel_msg.linear.y = 0; 
+  vel_msg.linear.z = 0; 
+  vel_msg.angular.x = 0;
+  vel_msg.angular.y = 0;
+
+  // Continue to track the same person, or find a new person to track (if any)
+  if(s != -1){
+    if(msg.bodies[s].isTracked == 0){
+      s = -1;
+      ROS_INFO_NAMED("personFollower", "personFollower: tracking ended");
     }
-	// Set the velocities
-    if(s != -1){
-		//count = 0;
-		// Set the angular velocity
-	    	ROS_INFO_STREAM(msg.bodies[s].jointPositions[0].position.z);
-		if(msg.bodies[s].jointPositions[0].position.x >= 0.2){
-			vel_msg.angular.z = 0.18;
-			aboveAngle = true;
-		} 
-		else if(msg.bodies[s].jointPositions[0].position.x <= -0.2){
-			vel_msg.angular.z = -0.18;
-			aboveAngle = true;
-		}
-		else{
-			vel_msg.angular.z = 0;	
-			aboveAngle = false;
-			//personPos_pub.publish(vel_msg);
-		}
-		// Set the forward velocity
-		if(msg.bodies[s].jointPositions[0].position.z >= 1.5){
-			vel_msg.linear.x = 0.18; //msg.bodies[s].jointPositions[3].position.z/7;
-			aboveLength = true;
-			underLenCount = 0;
-		}
-		else if(msg.bodies[s].jointPositions[0].position.z < 1.4){
-			++underLenCount;
-			if(underLenCount > 3){
-				vel_msg.linear.x = 0;
-				aboveLength = false;
-				underLenCount = 0;
-			}
-			//personPos_pub.publish(vel_msg);
-		}
+  }
+  if(s == -1){
+    //if(count > 10){
+    vel_msg.linear.x = 0;
+    vel_msg.angular.z = 0;
+    //}
+    aboveAngle = false;
+    aboveLength = false;
+    for(int i = 0; i < 6; i++){
+      if(msg.bodies[i].isTracked == 1){
+        s = i;
+        ROS_INFO_NAMED("personFollower", "personFollower: tracking new person");
+        break;
+      }
     }
+  }
+  // Set the velocities
+  if(s != -1){
+    //count = 0;
+    // Set the angular velocity
+    //ROS_INFO_STREAM(msg.bodies[s].jointPositions[0].position.z);
+    if(msg.bodies[s].jointPositions[0].position.x >= 0.2){
+      vel_msg.angular.z = 0.18;
+      aboveAngle = true;
+    } 
+    else if(msg.bodies[s].jointPositions[0].position.x <= -0.2){
+      vel_msg.angular.z = -0.18;
+      aboveAngle = true;
+    }
+    else{
+      vel_msg.angular.z = 0;	
+      aboveAngle = false;
+      //personPos_pub.publish(vel_msg);
+    }
+    // Set the forward velocity
+    if(msg.bodies[s].jointPositions[0].position.z >= 1.5){
+      vel_msg.linear.x = 0.18; //msg.bodies[s].jointPositions[3].position.z/7;
+      aboveLength = true;
+      underLenCount = 0;
+    }
+    else if(msg.bodies[s].jointPositions[0].position.z < 1.4){
+      ++underLenCount;
+      if(underLenCount > 3){
+        vel_msg.linear.x = 0;
+        aboveLength = false;
+        underLenCount = 0;
+      }
+      //personPos_pub.publish(vel_msg);
+    }
+  }
 
-	// Publish if there is a new state
-	ROS_INFO_STREAM(prevAboveAngle);
-	ROS_INFO_STREAM(aboveAngle);
-	if(prevAboveAngle != aboveAngle || prevAboveLength != aboveLength || s == -1){
-		ROS_INFO_STREAM("Publishing message");
-		personPos_pub.publish(vel_msg);
-	}
-	prevAboveAngle = aboveAngle;
-	prevAboveLength = aboveLength;
+  // Publish if there is a new state
+  ROS_INFO_STREAM(prevAboveAngle);
+  ROS_INFO_STREAM(aboveAngle);
+  if(prevAboveAngle != aboveAngle || prevAboveLength != aboveLength || s == -1){
+    ROS_INFO_STREAM("Publishing message");
+    personPos_pub.publish(vel_msg);
+  }
+  prevAboveAngle = aboveAngle;
+  prevAboveLength = aboveLength;
 }
 
 
 int main(int argc,char **argv){
-	ros::init(argc,argv,"personFollower");
-	ros::NodeHandle n;
+  ros::init(argc,argv,"personFollower");
+  ros::NodeHandle n;
 
-	// Set up watchdog
-	signal(SIGALRM, watchdog);
-	alarm(3);
+  // Set up watchdog
+  signal(SIGALRM, watchdog);
+  alarm(3);
 
-	// Set up velocity command publisher
-	personPos_pub = n.advertise<geometry_msgs::Twist>("RosAria/cmd_vel",1);
+  // Set up velocity command publisher
+  personPos_pub = n.advertise<geometry_msgs::Twist>("RosAria/cmd_vel",1);
 
-	// Subsribe to topic "bodyArray" published by k2_klient package node startBody.cpp
-	ros::Subscriber bodies_sub = n.subscribe("head/kinect2/bodyArray", 1, bodies_sub_cb); 
+  // Subsribe to topic "bodyArray" published by k2_klient package node startBody.cpp
+  ros::Subscriber bodies_sub = n.subscribe("head/kinect2/bodyArray", 1, bodies_sub_cb); 
+  ros::Subscriber pose_sub = n.subscribe("Rosaria/pose", 1, pose_sub_cb); 
 
-	ros::Rate loop_rate(6); //0.1
+  ros::Rate loop_rate(6); //0.1
 
-	ROS_INFO_NAMED("personFollower", "personFollower: Running ROS node...");
-	while (ros::ok()){
-		ros::spinOnce();
-		loop_rate.sleep();
-	}
-	//ros::spin();
+  ROS_INFO_NAMED("personFollower", "personFollower: Running ROS node...");
+  while (ros::ok()){
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+  //ros::spin();
 
-	ROS_INFO_NAMED("personFollower",  "personFollower: Quitting... \n" );
+  ROS_INFO_NAMED("personFollower",  "personFollower: Quitting... \n" );
 
-	return 0;
+  return 0;
 }   
