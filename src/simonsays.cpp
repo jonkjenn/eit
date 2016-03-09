2e4df22f1bb38d930ddf0df435139d2619b7079f
christoo <christoo@users.noreply.github.com>
2016-03-09 13:13:19 +0100

    1000 commit

--- src/simonsays.cpp
+++ src/simonsays.cpp
@@ -9,33 +9,52 @@
 
 void bodies_sub_cb(const k2_client::BodyArray msg)
 {
-  //gjenkjenne gesturer fra kinnect: 
   
-
-  //happy or neutral
+    
+   //gjenkjenne gesturer fra kinnect:
+    
   for (const auto& body : msg.bodies){
       
+    //Ser om personen er glad
+      
     if (body.expressions.happy){
+        
       ROS_INFO("The user eats happymeal ");
+        
     }
       
+    //Ser om personen er nøytral
+      
     else if (body.expressions.neutral){
+        
       ROS_INFO("The user is neutral ");
+        
     }
-    
+    //Ser om personen har øynene lukket
+      
     if(body.activities.eyeleftClosed and body.activities.eyeRightClosed){
+        
       ROS_INFO("Eye closed af");
+        
     }
-  
+    //ser om personen har på seg briller
+      
     if(body.appearance.wearingGlasses){
+        
       ROS_INFO("Wearing Glases");
+        
     }
+      
   }
-    
+
 }
+
+
+
 
 int main(int argc, char **argv)
 {
+    
   /  ros::init(argc, argv, "listener");
   ros::NodeHandle n;
 
