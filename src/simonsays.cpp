e8f28e070122a438d44ccaca65f61f6403d2d537
christoo <christoo@users.noreply.github.com>
2016-03-09 13:09:35 +0100

    second commit

--- src/simonsays.cpp
+++ src/simonsays.cpp
@@ -14,9 +14,11 @@ void bodies_sub_cb(const k2_client::BodyArray msg)
 
   //happy or neutral
   for (const auto& body : msg.bodies){
+      
     if (body.expressions.happy){
       ROS_INFO("The user eats happymeal ");
     }
+      
     else if (body.expressions.neutral){
       ROS_INFO("The user is neutral ");
     }
@@ -29,57 +31,19 @@ void bodies_sub_cb(const k2_client::BodyArray msg)
       ROS_INFO("Wearing Glases");
     }
   }
+    
 }
 
 int main(int argc, char **argv)
 {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line.
-   * For programmatic remappings you can use a different version of init() which takes
-   * remappings directly, but for most command-line programs, passing argc and argv is
-   * the easiest way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
-  ros::init(argc, argv, "listener");
-
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
+  /  ros::init(argc, argv, "listener");
   ros::NodeHandle n;
 
-  /**
-   * The subscribe() call is how you tell ROS that you want to receive messages
-   * on a given topic.  This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing.  Messages are passed to a callback function, here
-   * called chatterCallback.  subscribe() returns a Subscriber object that you
-   * must hold on to until you want to unsubscribe.  When all copies of the Subscriber
-   * object go out of scope, this callback will automatically be unsubscribed from
-   * this topic.
-   *
-   * The second parameter to the subscribe() function is the size of the message
-   * queue.  If messages are arriving faster than they are being processed, this
-   * is the number of messages that will be buffered up before beginning to throw
-   * away the oldest ones.
-   */
-// %Tag(SUBSCRIBER)%
   ros::Subscriber sub = n.subscribe("head/kinect2/bodyArray", 1000, bodies_sub_cb);
-// %EndTag(SUBSCRIBER)%
 
-  /**
-   * ros::spin() will enter a loop, pumping callbacks.  With this version, all
-   * callbacks will be called from within this thread (the main one).  ros::spin()
-   * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
-   */
-// %Tag(SPIN)%
-  ros::spin();
-// %EndTag(SPIN)%
+
+    ros::spin();
 
   return 0;
 }
-// %EndTag(FULLTEXT)%
+
