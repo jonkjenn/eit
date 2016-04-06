c28c1530614e5383a2c823a5222dde33199aa9d5
christoo <christoo@stud.ntnu.no>
2016-04-06 11:14:26 +0200

    added 3 gestures

--- src/gameGestures.cpp
+++ src/gameGestures.cpp
@@ -12,7 +12,6 @@ bool gestureLiftRightFoot(const k2_client::Body& body){
     }
 }
 
-
 bool gesturFlexnes(const k2_client::Body& body){
     const auto& elbowRight = body.jointPositions[9];
     const auto& elbowLeft = body.jointPositions[5];
@@ -22,17 +21,23 @@ bool gesturFlexnes(const k2_client::Body& body){
     const auto& wristLeft  = body.jointPositions[6];
     
     
-    if( (abs(elbowRight.position.y - shoulderRight.position.y)<0.1) && (abs(wristRight.position.x == elbowRight.position.x)<0.1) && (abs(elbowLeft.position.y == shoulderLeft.position.y)<0.1) && (abs(wristLeft.position.x == elbowLeft.position.x)<0.1)) {
+    if( (abs(elbowRight.position.y - shoulderRight.position.y)<0.1) && (abs(wristRight.position.x - elbowRight.position.x)<0.1) && (abs(elbowLeft.position.y - shoulderLeft.position.y)<0.1) && (abs(wristLeft.position.x - elbowLeft.position.x)<0.1)) {
         return true;
     }
     else{
         return false;
     }
-    
-    
 }
 
 
-bool gestureHandsUp(const k2_client::Body:: body){
+bool gestureStop(const k2_client::Body:: body){
+    const auto& handRight = body.jointPositions[11];
+    const audo& shoulderRight = body.jointPositions[8];
     
+    if(abs(handRight.position.y - shoulderRight.position.y )<0.1  &&  (handRight.position.z < shoulderRight.position.z)){
+        return true
+    }
+    else {
+        return false
+    }
 }
