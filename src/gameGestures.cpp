488918337b4549598c81c46289bc1a3f1f3b4fb3
christoo <christoo@stud.ntnu.no>
2016-04-06 10:43:29 +0200

    Added two gestures bitch

--- src/gameGestures.cpp
+++ src/gameGestures.cpp
@@ -3,7 +3,7 @@ bool gestureLiftRightFoot(cinst k2_client::Body& body){
     const auto& kneeLeft = body.jointPositions[13];
     
     //Sjekker om høyre kne er høyere enn venstre kne
-    if(kneeright.position.y > (kneeLeft.position.y) ){
+    if(kneeright.position.y > ((kneeLeft.position.y)0.1) ){
         return true;
     }
     
@@ -11,9 +11,6 @@ bool gestureLiftRightFoot(cinst k2_client::Body& body){
         return false;
     }
 }
-
-
-
 
 bool gesturFlexnes(cinst k2_client::Body& body){
     const auto& elbowRight = body.jointPositions[9];
@@ -24,13 +21,12 @@ bool gesturFlexnes(cinst k2_client::Body& body){
     const auto& wristLeft  = body.jointPositions[6];
     
     
-    if( (abs(elbowRight.position.y - shoulderRight.position.y)<0.1) && (wristRight.position.x == elbowRight.position.x) && (elbowLeft.position.y == shoulderLeft.position.y) && (wristLeft.position.x == elbowLeft.position.x)) {
+    if( (abs(elbowRight.position.y - shoulderRight.position.y)<0.1) && (abs(wristRight.position.x == elbowRight.position.x)<0.1) && (abs(elbowLeft.position.y == shoulderLeft.position.y)<0.1) && (abs(wristLeft.position.x == elbowLeft.position.x)<0.1)) {
         return true;
     }
     else{
         return false;
     }
-        
+    
     
 }
-
