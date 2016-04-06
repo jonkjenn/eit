badb5e7b2b62de7b4511ea498c98aac6a861dc91
gdsokjer <gdsokjer@stud.ntnu.no>
2016-04-06 14:31:44 +0200

    Update personFollower.cpp

--- src/personFollower.cpp
+++ src/personFollower.cpp
@@ -1,199 +1,261 @@
+//#include <math.h>
+
 #include "ros/ros.h"
 #include <k2_client/k2_client.h>
 #include <k2_client/BodyArray.h>
 #include <geometry_msgs/Twist.h>
-#include <sensor_msgs/LaserScan.h>
-#include <rosaria/BumperState.h>
-#include <nav_msgs/Odometry.h>
 #include <tf/transform_listener.h>
-#include <signal.h>
-#include <string>
-//#include "pid.h"
+#include <deque>
 
-using namespace std;
-//#include <algorithm> // min and max
 
-ros::Publisher personPos_pub;
-geometry_msgs::Twist vel_msg;
-geometry_msgs::Twist prev_vel_msg;
+int start_wave[6] = {2,2,2,2,2,2};
+int count_wave[6] = {0,0,0,0,0,0};
+int lastTime_wave1[6] = {0,0,0,0,0,0};
+int lastTime_wave2[6] = {0,0,0,0,0,0};
+int time_wave[6] = {0,0,0,0,0,0};
 
-ros::Time prev_time;
-float prev_vel_z;
+int s=-1;
 
-int checkCount = 0;
-int s = -1;
-bool aboveAngle = false;
-bool aboveLength = false;
-bool prevAboveAngle = false;
-bool prevAboveLength = false;
-
-bool stopped = false;
-
-//int count = 0;
-int underLenCount = 0;
-
-void watchdog(int sig){
-	vel_msg.linear.x = 0; 
-	vel_msg.linear.y = 0; 
-	vel_msg.linear.z = 0; 
-	vel_msg.angular.x = 0;
-	vel_msg.angular.y = 0;
-	vel_msg.angular.z = 0;
-	personPos_pub.publish(vel_msg);
+bool gestureTestMidle(double y1, double x1, double y2, double x2){
+    if(abs(x1 - x2) <= 0.05*sqrt(pow((y1-y2),2) + pow((x1-x2),2))){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
-bool gestureTestMidle(const k2_client::Body& body){
-	const auto& shoulderRight = body.jointPositions[8];
-	const auto& handRight = body.jointPositions[11];
-	const auto& shoulderLeft = body.jointPositions[4];
-	const auto& handLeft = body.jointPositions[7];
-
-	if(abs(shoulderRight.position.y - handRight.position.y) <= 0.2 && abs(shoulderLeft.position.y - handLeft.position.y) <= 0.2){
-		return true;
-	}
-	else{
-		return false;
-	}
+bool gestureTestRight(double y1, double x1, double y2, double x2){
+    if(x1 < x2 && abs(x1 - x2) > 0.05*sqrt(pow((y1-y2),2) + pow((x1-x2),2))){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
-void laser_sub_cb(const sensor_msgs::LaserScan msg){
+bool gestureTestAbove(double y1, double x1, double y2, double x2){
+    if(y1 > y2 && abs(y1 - y2) > 0.05*sqrt(pow((y1-y2),2) + pow((x1-x2),2))){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
-void bumper_state_sub_cb(const rosaria::BumperState msg){
-	//ROS_INFO_STREAM("Bumpers: " << msg.front_bumpers.size());
-	for(bool b:msg.front_bumpers){
-		//ROS_INFO_STREAM("Bumper: " << b);
-		if(b){
-			stopped = true;
-			return;
-		}
-	}
-	stopped = false;
+
+
+
+
+
+void gestureWave(const k2_client::Body& body, int bodyNumber){
+    
+    const auto& elbowRight = body.jointPositions[9];
+    const auto& handRight = body.jointPositions[11];
+    
+    time_wave[bodyNumber] ++;
+    if (time_wave[bodyNumber]==1) {
+        if(s != -1 && gestureTestMidle(handRight.position.y, handRight.position.x, elbowRight.position.y, elbowRight.position.x) == false && gestureTestAbove(handRight.position.y, handRight.position.x, elbowRight.position.y, elbowRight.position.x)){
+            ROS_INFO("Startet");
+            if(gestureTestRight(handRight.position.y, handRight.position.x, elbowRight.position.y, elbowRight.position.x)){
+                start_wave[bodyNumber] = 1;
+            }
+            else{
+                start_wave[bodyNumber] = 0;
+            }
+            lastTime_wave1[bodyNumber]=time_wave[bodyNumber];
+            count_wave[bodyNumber]++;
+        }
+    }
+    else{
+        if(s != -1 && gestureTestAbove(handRight.position.y, handRight.position.x, elbowRight.position.y, elbowRight.position.x)){
+            if(gestureTestMidle(handRight.position.y, handRight.position.x, elbowRight.position.y, elbowRight.position.x) == false){
+                if(start_wave[bodyNumber] == 2){
+                    ROS_INFO("Startet");
+                    if(gestureTestRight(handRight.position.y, handRight.position.x, elbowRight.position.y, elbowRight.position.x)){
+                        start_wave[bodyNumber] = 1;
+                    }
+                    else{
+                        start_wave[bodyNumber] = 0;
+                    }
+                    lastTime_wave1[bodyNumber]=time_wave[bodyNumber];
+                    count_wave[bodyNumber]++;
+                    ROS_INFO("Fant 1");
+                }
+                else if(start_wave[bodyNumber] == 0){
+                    if(gestureTestRight(handRight.position.y, handRight.position.x, elbowRight.position.y, elbowRight.position.x)){
+                        start_wave[bodyNumber] = 1;
+                        lastTime_wave2[bodyNumber]=time_wave[bodyNumber];
+                        count_wave[bodyNumber]++;
+                        ROS_INFO("Fant 1");
+                    }
+                }
+                else{
+                    if(gestureTestRight(elbowRight.position.y, elbowRight.position.x, handRight.position.y, handRight.position.x)){
+                        start_wave[bodyNumber] = 0;
+                        lastTime_wave2[bodyNumber]=time_wave[bodyNumber];
+                        count_wave[bodyNumber] ++;
+                        ROS_INFO("Fant 1");
+                    }
+                }
+            }
+        }
+    }
+    if(count_wave[bodyNumber]==0){
+        time_wave[bodyNumber] = 0;
+    }
+    if(time_wave[bodyNumber] > 60 && count_wave[bodyNumber] < 3){
+        if (count_wave[bodyNumber] == 2) {
+            time_wave[bodyNumber] -=(lastTime_wave2[bodyNumber]-1);
+            lastTime_wave2[bodyNumber] = 0;
+            lastTime_wave1[bodyNumber] = 1;
+        }
+        else {
+            lastTime_wave1[bodyNumber] = 0;
+            time_wave[bodyNumber] = 0;
+            start_wave[bodyNumber]=2;
+        }
+        if (count_wave[bodyNumber] > 0) {
+            count_wave[bodyNumber] --;
+        }
+    }
 }
 
-void bodies_sub_cb(const k2_client::BodyArray msg){
-	//ROS_INFO_NAMED("personFollower", "personFollower: Received bodyArray");
 
-	alarm(1);
-	//++count;
+bool gestureStop(const k2_client::Body& body){
+    const auto& shoulderRight = body.jointPositions[8];
+    const auto& handRight = body.jointPositions[11];
+    
+    if(handRight.position.z < (shoulderRight.position.z-0.2) && body.handRightState==2){
+        ROS_INFO("Leter etter stop gesture");
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 
-	vel_msg.linear.y = 0; 
-	vel_msg.linear.z = 0; 
-	vel_msg.angular.x = 0;
-	vel_msg.angular.y = 0;
 
-	if(stopped){
-		personPos_pub.publish(vel_msg);
-		return;
-	}
 
-	// Continue to track the same person, or find a new person to track (if any)
-	if(s != -1){
-		if(msg.bodies[s].isTracked == 0){
-			s = -1;
-			checkCount = 1;
-			//ROS_INFO_NAMED("personFollower", "personFollower: tracking ended");
-		}
-	}
-	if(s == -1){
-		//if(count > 10){
-		vel_msg.linear.x = 0;
-		vel_msg.angular.z = 0;
-		//}
-		aboveAngle = false;
-		aboveLength = false;
-		for(int i = 0; i < 6; i++){
-			if(msg.bodies[i].isTracked && gestureTestMidle(msg.bodies[i])){
-				s = i;
-				checkCount = 0;
-				//ROS_INFO_NAMED("personFollower", "personFollower: tracking new person");
-				break;
-			}
-		}
-	}
-	// Set the velocities
-	if(s != -1){
-		//count = 0;
-		// Set the angular velocity
-		//ROS_INFO_STREAM(msg.bodies[s].jointPositions[0].position.z);
-		if(msg.bodies[s].jointPositions[0].position.x >= 0.2){
-			vel_msg.angular.z = 0.18;
-			aboveAngle = true;
-		} 
-		else if(msg.bodies[s].jointPositions[0].position.x <= -0.2){
-			vel_msg.angular.z = -0.18;
-			aboveAngle = true;
-		}
-		else{
-			vel_msg.angular.z = 0;	
-			aboveAngle = false;
-			//personPos_pub.publish(vel_msg);
-		}
-		// Set the forward velocity
-		if(msg.bodies[s].jointPositions[0].position.z >= 1.5){
-			vel_msg.linear.x = 0.18; //msg.bodies[s].jointPositions[3].position.z/7;
-			aboveLength = true;
-			underLenCount = 0;
-		}
-		else if(msg.bodies[s].jointPositions[0].position.z < 1.5){
-			++underLenCount;
-			if(underLenCount > 3){
-				vel_msg.linear.x = 0;
-				aboveLength = false;
-				underLenCount = 0;
-			}
-			//personPos_pub.publish(vel_msg);
-		}
-	}
-	if(checkCount > 0 && checkCount < 2*loop_rate){
-    		checkCount++;
-    		vel_msg = prev_vel_msg;
-	}
-  	else if(checkCount >= 2*loop_rate){
-    	checkCount = 0;
-  	}
-	
-	// Publish if there is a new state
-	//ROS_INFO_STREAM(prevAboveAngle);
-	//ROS_INFO_STREAM(aboveAngle);
-	if(prevAboveAngle != aboveAngle || prevAboveLength != aboveLength || s == -1){
-		//ROS_INFO_STREAM("Publishing message");
-		prev_vel_msg = vel_msg;
-		personPos_pub.publish(vel_msg);
-	}
-	prevAboveAngle = aboveAngle;
-	prevAboveLength = aboveLength;
+namespace Constants {
+    int bodyArrayHistoryMaxSize = 60;
+    int bodyArrayNewMaxSize = 10;
+}
+
+std::deque<k2_client::BodyArray> bodyArrayHistory;
+
+std::deque<k2_client::BodyArray> bodyArrayNew;
+
+
+// Subscriber callback function for recieving BodyArray msg from k2_client
+void gesture_sub_cb(const k2_client::BodyArray msg){
+    //ROS_INFO_NAMED("personGesture", "personGesture: Received bodyArray");
+    
+    bodyArrayNew.push_back(msg);
+    if (bodyArrayNew.size() >= Constants::bodyArrayNewMaxSize){
+        bodyArrayNew.pop_back();
+    }
+    
 }
 
 
 int main(int argc,char **argv){
-	ros::init(argc,argv,"personFollower");
-	ros::NodeHandle n;
-
-	prev_time = ros::Time::now();
-
-	// Set up watchdog
-	signal(SIGALRM, watchdog);
-	alarm(3);
-
-	// Set up velocity command publisher
-	personPos_pub = n.advertise<geometry_msgs::Twist>("RosAria/cmd_vel",1);
-
-	// Subsribe to topic "bodyArray" published by k2_klient package node startBody.cpp
-	ros::Subscriber bodies_sub = n.subscribe("head/kinect2/bodyArray", 1, bodies_sub_cb); 
-	ros::Subscriber laser_sub = n.subscribe("RosAria/S3Series_1_laserscan", 1, laser_sub_cb); 
-	ros::Subscriber bumper_sub = n.subscribe("RosAria/bumper_state", 1, bumper_state_sub_cb);
-
-	ros::Rate loop_rate(30); //0.1
-
-	ROS_INFO_NAMED("personFollower", "personFollower: Running ROS node...");
-	while (ros::ok()){
-		ros::spinOnce();
-		loop_rate.sleep();
-	}
-	//ros::spin();
-
-	ROS_INFO_NAMED("personFollower",  "personFollower: Quitting... \n" );
-
-	return 0;
-}   
+    ros::init(argc,argv,"personGesture");
+    ros::NodeHandle n;
+    
+    // Subsribe to topic "bodyArray" published by k2_klient package node startBody.cpp
+    ros::Subscriber gesture_sub = n.subscribe("head/kinect2/bodyArray", 1000, gesture_sub_cb);
+    
+    ros::Rate loop_rate(15); //0.1
+    
+    // Run the ROS node
+    ROS_INFO_NAMED("personGesture", "personGesture: Running ROS node...");
+    
+    bool alive = false;
+    int state = 0;
+    int afk = 0;
+    int count_stop = 0;
+    
+    while (ros::ok()){
+        ros::spinOnce();
+        loop_rate.sleep();
+        if(alive){
+            for(const auto& bodyArray : bodyArrayNew){
+                for(int i = 0; i <= 6; i++){
+                    s = -1;
+                    if(bodyArray.bodies[i].isTracked){
+                        s = i;
+                    }
+                    gestureWave(bodyArray.bodies[i],i);
+                    if (count_wave[i] >2) {
+                        state = 1;
+                        alive = false;
+                        ROS_INFO("Wave funnet");
+                        for (int j=0; j <= 6; j++) {
+                            start_wave[j] = 2;
+                            count_wave[j] = 0;
+                            lastTime_wave1[j]=0;
+                            lastTime_wave2[j]=0;
+                            time_wave[j] = 0;
+                        }
+                        break;
+                    }
+                }
+                if (!alive) {
+                    break;
+                }
+                bodyArrayHistory.push_front(bodyArray);
+            }
+            bodyArrayNew.clear();
+            if (!alive) {
+                bodyArrayHistory.clear();
+            }
+        }
+        else{
+            for(const auto& bodyArray : bodyArrayNew){
+                for(int i = 0; i <= 6; i++){
+                    if(bodyArray.bodies[i].isTracked){
+                        s = i;
+                        break;
+                    }
+                }
+                if(bodyArray.bodies[s].isTracked){
+                    afk=0;
+                    if(gestureStop(bodyArray.bodies[s])){
+                        count_stop++;
+                    }
+                }
+                else{
+                    afk++;
+                }
+                bodyArrayHistory.push_front(bodyArray);
+                const auto& historyBack = bodyArrayHistory.back();
+                if(bodyArrayHistory.size() >= Constants::bodyArrayHistoryMaxSize && count_stop < 30){
+                    if(gestureStop(historyBack.bodies[s]) && count_stop > 0){
+                        count_stop--;
+                    }
+                    bodyArrayHistory.pop_back();
+                }
+                if (count_stop >= 30 || afk > 90) {
+                    state = 0;
+                    alive = true;
+                    count_stop = 0;
+                    afk=0;
+                    bodyArrayHistory.clear();
+                    ROS_INFO("Follow stopped");
+                    break;
+                }
+            }
+            bodyArrayNew.clear();
+        }
+        ROS_INFO_STREAM(bodyArrayHistory.size());
+        
+        if (bodyArrayHistory.size() >= Constants::bodyArrayHistoryMaxSize){
+            ROS_INFO("Fail?");
+            bodyArrayHistory.pop_back();
+            ROS_INFO("Nope");
+        }
+    }
+    ROS_INFO_NAMED("personGesture",  "personGesture: Quitting... \n" );
+    return 0;
+}
